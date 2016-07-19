b3192d1f3436d030d5fd2f60d3d6894b5e947b07
ccoder22 <foreignchain295@gmail.com>
2016-07-19 14:30:09 -0600

    Added the style.css, head-utils, and index to the navbar and to the yearbook

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("lib/head-utils.php");?>
+
+</head>
+
+<title>YearBook Homepage</title>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contacts<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Emails</a></li>
+                            <li><a href="#">Facebook link</a></li>
+                            <li><a href="#">Send Pics&Vids</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Year/Class<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Grads</a></li>
+                            <li><a href="#">Regulars</a></li>
+                            <li><a href="#">Baby Pics</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Staff<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Teachers</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Events<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Dances/Homecoming/Prom</a></li>
+                            <li><a href="#">College Trips</a></li>
+                            <li><a href="#">Band Trip</a></li>
+                            <li><a href="#">ROTC Trips</a></li>
+                            <li><a href="#">Girls Basketball</a></li>
+                            <li><a href="#">GearUp</a></li>
+                            <li><a href="#">Graduation</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Clubs<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Student Council</a></li>
+                            <li><a href="#">Chess Club</a></li>
+                            <li><a href="#">FFA</a></li>
+                            <li><a href="#">Dine Club</a></li>
+                            <li><a href="#">NHS</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Sports<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Baseball</a></li>
+                            <li><a href="#">Boys Basketball</a></li>
+                            <li><a href="#">Girls Basketball</a></li>
+                            <li><a href="#">Cheer leading</a></li>
+                            <li><a href="#">Cross Country</a></li>
+                            <li><a href="#">FootBall</a></li>
+                            <li><a href="#">Golf</a></li>
+                            <li><a href="#">SoftBall</a></li>
+                            <li><a href="#">Track</a></li>
+                            <li><a href="#">VolleyBall</a></li>
+                            <li><a href="#">Wrestling</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"> </a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departments<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Art</a></li>
+                            <li><a href="#">Band</a></li>
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">ROTC</a></li>
+                            <li><a href="#">Math</a></li>
+                            <li><a href="#">Science</a></li>
+                            <li><a href="#">Social Studies</a></li>
+                            <li><a href="#">Electives</a></li>
+                            <li><a href="#">Exceptional Programs</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
+
+<body id="background" background="https://files.slack.com/files-pri/T0ZS050BX-F1RQC4144/11947546_900474536713484_467584626795916643_n.jpg">
+
+<div id="container">
+
+
+
+</div>
+
+</body>
+
+</html>
---
+++ lib/head-utils.php
@@ -0,0 +1,27 @@
+<meta charset="utf-8">
+<title>YearBook Homepage</title>
+
+<script src="http://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+      crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+      integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+      crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+        integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+        crossorigin="anonymous"></script>
+
+<!-- FONT AWESOME-->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"
+      integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
+      crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="lib/css/style.css"/>
