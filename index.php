ad1a8bb99a08aca994b43f4f725043cabc771ca1
KSC-4 <keachico3@gmail.com>
2016-07-20 10:10:25 -0600

    added  jumbotron for the student life thmbnail

--- index.php
+++ index.php
@@ -12,9 +12,14 @@
 <body id="background" background="https://files.slack.com/files-pri/T0ZS050BX-F1RQC4144/11947546_900474536713484_467584626795916643_n.jpg">
     <?php require_once("lib/navbar.php");?>
 
-<div id="container">
-
-
+<div class="jumbotron">
+    <h1>Student Life</h1>
+    <div class="row">
+        <div class="col-md-9">
+            <a href="#" class="thumbnail">
+                <img src="..." alt="...">
+            </a>
+        </div>
 
 </div>
 
