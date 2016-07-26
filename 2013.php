05bb13aa5c0d847a82f675981a2339e6a538ad4c
Charles Sandidge <sandidgec@gmail.com>
2016-07-26 10:16:49 -0600

    added jumbotron and pictures into 2013 page

--- 2013.php
+++ 2013.php
@@ -6,58 +6,64 @@
 </head>
 
 <body>
-<div class="jumbotron">
-    <h1>2013</h1>
-    <p>...</p>
-</div>
-<p>lerp burp merp
-<div class="container">
-       <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg"
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <div class="thumbnail">
-                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
-            </div>
-        </div>
-</div>
+ <div class="container">
+      <div class="jumbotron">
+        <h1>2013</h1>
+        <p>sloths are cute and sloths are me</p>
+      </div>
+     </div>
+   <div class="row">
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+   </div>
+   <div class="row">
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+   </div>
+   <div class="row">
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+       <div class="col-md-4 col-sm-6">
+           <div class="thumbnail">
+               <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg" class="img-circle">
+           </div>
+       </div>
+   </div>
+ </div>
 
 
 </body>
