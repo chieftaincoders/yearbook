9375383a3ad3eaeda913f455234636048f34218b
Charles Sandidge <sandidgec@gmail.com>
2016-07-26 09:25:49 -0600

    added jumbotron and pictures into 2013 page

--- 2013.php
+++ 2013.php
@@ -6,8 +6,60 @@
 </head>
 
 <body>
+<div class="jumbotron">
+    <h1>2013</h1>
+    <p>...</p>
+</div>
+<p>lerp burp merp
+<div class="container">
+       <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg"
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+            <div class="thumbnail">
+                <img src="https://kstr.files.wordpress.com/2013/06/upside-down-kermie.jpg">
+            </div>
+        </div>
+</div>
 
-<p>lerp burp merp slurp</p>
+
 </body>
 
 </html>
