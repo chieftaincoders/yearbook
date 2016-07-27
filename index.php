7855b81cb72b3b5ebeef6e176c7947ea7ec78bb0
ccoder22 <foreignchain295@gmail.com>
2016-07-27 10:14:21 -0600

    cleaned up the navbar and index

--- index.php
+++ index.php
@@ -1,63 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require_once("lib/head-utils.php");?>
-
-</head>
-
-<title>YearBook Homepage</title>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <?php require_once("lib/head-utils.php");?>
+    </head>
+    <title>YearBook Homepage</title>
 
 
 
-<body id="background" background="https://files.slack.com/files-pri/T0ZS050BX-F1RQC4144/11947546_900474536713484_467584626795916643_n.jpg">
-<?php require_once("lib/navbar.php");?>
+    <body id="background" background="https://files.slack.com/files-pri/T0ZS050BX-F1RQC4144/11947546_900474536713484_467584626795916643_n.jpg">
+    <?php require_once("lib/navbar.php");?>
 
-<div class="jumbotron">
-    <h1>Student Life</h1>
-    <h2>Damn</h2>
+    <div class="jumbotron">
+        <h1>Student Life</h1>
+        <h2>Damn</h2>
+        <div class="row">
+            <div class="col-md-9">
+                <a href="#" class="thumbnail">
+                    <img src="..." alt="...">
+                </a>
+            </div>
+        </div>
+    </div>
+
     <div class="row">
-        <div class="col-md-9">
-            <a href="#" class="thumbnail">
-                <img src="..." alt="...">
-            </a>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-3">
-        <div class="media">
-            <div class="media-left media-middle">
-                <a href="#">
-                    <img class="media-object" src="..." alt="...">
-                </a>
-            </div>
-            <div class="media-body">
-                <h4 class="page-header">Principal</h4>
-                    <p></p>
+        <div class="col-md-3">
+            <div class="media">
+                <div class="media-left media-middle">
+                    <a href="#">
+                        <img class="media-object" src="..." alt="...">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="page-header">Principal</h4>
+                        <p></p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <div class="col-md-3 blue">
-        <div class="media">
-            <div class="media-left media-middle">
-                <a href="#">
-                    <img class="media-object purple" src="/yearbook/lib/images/chieftains.png" alt="thisIsWhereAnImageGoes">
-                </a>
-            </div>
-            <div class="media-body purple">
-                <h3>"Once A Chieftain Always A Chieftain"</h3>
-
+        <div class="col-md-3 blue">
+            <div class="media">
+                <div class="media-left media-middle">
+                    <a href="#">
+                        <img class="media-object purple" src="/yearbook/lib/images/chieftains.png" alt="thisIsWhereAnImageGoes">
+                    </a>
+                </div>
+                <div class="media-body purple">
+                    <h3>"Once A Chieftain Always A Chieftain"</h3>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="container col-md-2">
-    <h2>Information For Yearbooks</h2>
-    <p class="two">nsoiv uhns jhv dvndskjv dsvnfkv jv skjvs djkv dsvjsdvkdsn</p>
-</div>
+    <div class="container col-md-2">
+        <h2>Information For Yearbooks</h2>
+        <p class="two">nsoiv uhns jhv dvndskjv dsvnfkv jv skjvs djkv dsvjsdvkdsn</p>
+    </div>
 
 </body>
 
--- lib/navbar.php
+++ lib/navbar.php
@@ -1,114 +1,114 @@
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Home</a>
-        </div>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Home</a>
+            </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-phone-square" aria-hidden="true"></i>Contacts<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Emails</a></li>
-                            <li><a href="#">Facebook link</a></li>
-                            <li><a href="#">Send Pictures & Videos</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-graduation-cap" aria-hidden="true"></i>Year/Class<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="2015.php">2015</a></li>
-                            <li><a href="2014.php">2014</a></li>
-                            <li><a href="2013.php">2013</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>Staff<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Teachers</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>Events<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Dances/Homecoming/Prom</a></li>
-                            <li><a href="#">College Trips</a></li>
-                            <li><a href="#">Band Trip</a></li>
-                            <li><a href="#">ROTC Trips</a></li>
-                            <li><a href="#">Girls Basketball</a></li>
-                            <li><a href="#">GearUp</a></li>
-                            <li><a href="#">Graduation</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"></a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users" aria-hidden="true"></i>Clubs<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Student Council</a></li>
-                            <li><a href="#">Chess Club</a></li>
-                            <li><a href="#">FFA</a></li>
-                            <li><a href="#">Dine Club</a></li>
-                            <li><a href="#">NHS</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-futbol-o" aria-hidden="true"></i>Sports<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Baseball</a></li>
-                            <li><a href="#">Boys Basketball</a></li>
-                            <li><a href="#">Girls Basketball</a></li>
-                            <li><a href="#">Cheer leading</a></li>
-                            <li><a href="#">Cross Country</a></li>
-                            <li><a href="#">FootBall</a></li>
-                            <li><a href="#">Golf</a></li>
-                            <li><a href="#">SoftBall</a></li>
-                            <li><a href="#">Track</a></li>
-                            <li><a href="#">VolleyBall</a></li>
-                            <li><a href="#">Wrestling</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"> </a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-university" aria-hidden="true"></i>Departments<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="administration.php">Administration</a></li>
-                            <li><a href="english.php">English</a></li>
-                            <li><a href="#">Mathematics</a></li>
-                            <li><a href="#">Science</a></li>
-                            <li><a href="#">Social Studies</a></li>
-                            <li><a href="#">Electives</a></li>
-                            <li><a href="#">Special Education</a></li>
-                            <li><a href="#">Guidance</a></li>
-                            <li><a href="#">Support Staff</a></li>
-                        </ul>
-                    </li>
-                </ul>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-phone-square" aria-hidden="true"></i>Contacts<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Emails</a></li>
+                                <li><a href="#">Facebook link</a></li>
+                                <li><a href="#">Send Pictures & Videos</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-graduation-cap" aria-hidden="true"></i>Year/Class<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="2015.php">2015</a></li>
+                                <li><a href="2014.php">2014</a></li>
+                                <li><a href="2013.php">2013</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>Staff<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Teachers</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>Events<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Dances/Homecoming/Prom</a></li>
+                                <li><a href="#">College Trips</a></li>
+                                <li><a href="#">Band Trip</a></li>
+                                <li><a href="#">ROTC Trips</a></li>
+                                <li><a href="#">Girls Basketball</a></li>
+                                <li><a href="#">GearUp</a></li>
+                                <li><a href="#">Graduation</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"></a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users" aria-hidden="true"></i>Clubs<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Student Council</a></li>
+                                <li><a href="#">Chess Club</a></li>
+                                <li><a href="#">FFA</a></li>
+                                <li><a href="#">Dine Club</a></li>
+                                <li><a href="#">NHS</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-futbol-o" aria-hidden="true"></i>Sports<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Baseball</a></li>
+                                <li><a href="#">Boys Basketball</a></li>
+                                <li><a href="#">Girls Basketball</a></li>
+                                <li><a href="#">Cheer leading</a></li>
+                                <li><a href="#">Cross Country</a></li>
+                                <li><a href="#">FootBall</a></li>
+                                <li><a href="#">Golf</a></li>
+                                <li><a href="#">SoftBall</a></li>
+                                <li><a href="#">Track</a></li>
+                                <li><a href="#">VolleyBall</a></li>
+                                <li><a href="#">Wrestling</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"> </a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-university" aria-hidden="true"></i>Departments<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="administration.php">Administration</a></li>
+                                <li><a href="english.php">English</a></li>
+                                <li><a href="#">Mathematics</a></li>
+                                <li><a href="#">Science</a></li>
+                                <li><a href="#">Social Studies</a></li>
+                                <li><a href="#">Electives</a></li>
+                                <li><a href="#">Special Education</a></li>
+                                <li><a href="#">Guidance</a></li>
+                                <li><a href="#">Support Staff</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
