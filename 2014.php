acb475a98b0223a3a935121b627f4732c052de49
Charles Sandidge <sandidgec@gmail.com>
2016-08-10 09:01:09 -0600

    added more css

--- 2014.php
+++ 2014.php
@@ -1,13 +1,189 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require_once("lib/head-utils.php");?>
+    <?php require_once("lib/head-utils2.php");?>
 
 </head>
 
 <body>
 
-<p>derp derp derp derp derp</p>
-</body>
+<div id="landingPage">
+    <nav class="navbar navbar-transparent">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-transparent">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#pablo">Alumni 2014</a>
+            </div>
+            <div class="collapse navbar-collapse" id="example-navbar-transparent">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li>
 
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h1 class="ahText1 ahJumbo">CLASS OF 2014</h1>
+
+</div>
+
+
+
+
+
+<div class="container col-md-12">
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+
+            <span class="caption">Please work</span>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+</div>
+
+</body>
 </html>
--- 2015.php
+++ 2015.php
@@ -1,13 +1,189 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php require_once("lib/head-utils.php");?>
+    <?php require_once("lib/head-utils2.php");?>
 
 </head>
 
 <body>
 
-<p>merp merp merp merp merp</p>
-</body>
+<div id="landingPage">
+    <nav class="navbar navbar-transparent">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-transparent">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#pablo">Alumni 2015</a>
+            </div>
+            <div class="collapse navbar-collapse" id="example-navbar-transparent">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-facebook-square"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#pablo">
+                            <i class="fa fa-instagram"></i>
+                        </a>
+                    </li>
 
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <h1 class="ahText1 ahJumbo">CLASS OF 2015</h1>
+
+</div>
+
+
+
+
+
+<div class="container col-md-12">
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Amber Hender</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Alan Tails</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Rius ManGood</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Oshea Mae</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Robin</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Danyel John</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Meeerissa</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Spam Adam</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">KeaKorea</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Charles Trump Gump</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">DJKriegLegMeg</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">ManBun Sagor</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+
+            <span class="caption">Please work</span>
+        </div>
+
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+        <div class="col-md-2 col-xs-6">
+            <img src="lib/images/chieftains.png" class="img-responsive img-circle center-block">
+            <span class="caption">Please work</span>
+        </div>
+    </div>
+</div>
+
+</body>
 </html>
