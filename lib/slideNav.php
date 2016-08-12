99b0ecd75e0dfb9810a454f1ad741ee5ee34c7c9
KSC-4 <keachico3@gmail.com>
2016-08-12 08:41:31 -0600

    added names to football page and the slidenavbar

--- lib/slideNav.php
+++ lib/slideNav.php
@@ -12,7 +12,7 @@
         <ul>
 
             <li>
-                <a href="../index.php">
+                <a href="index.php">
                     <i class="fa fa-home"></i>
                     <span class="nav-text">Home</span>
                 </a>
@@ -22,9 +22,9 @@
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-phone-square" aria-hidden="true"></i>Contact Us<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="contactUs/email.php">Emails</a></li>
-                    <li><a href="../lib/contactUs/facebookLink.php">Facebook link</a></li>
-                    <li><a href="../lib/contactUs/sendPicturesAndVideos.php">Send Pictures & Videos</a></li>
+                    <li><a href="lib/contactUs/email.php">Emails</a></li>
+                    <li><a href="lib/contactUs/facebookLink.php">Facebook link</a></li>
+                    <li><a href="lib/contactUs/sendPicturesAndVideos.php">Send Pictures & Videos</a></li>
                 </ul>
             </li>
 
@@ -78,72 +78,72 @@ margin-left: 56px;top:3px;">
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-graduation-cap" aria-hidden="true"></i>Year/Class<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="../2015.php">2015</a></li>
-                    <li><a href="../2014.php">2014</a></li>
-                    <li><a href="../2013.php">2013</a></li>
+                    <li><a href="2015.php">2015</a></li>
+                    <li><a href="2014.php">2014</a></li>
+                    <li><a href="2013.php">2013</a></li>
                 </ul>
             </li>
 
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" aria-hidden="true"></i>Staff<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="../lib/staff/teachers.php">Teachers</a></li>
+                    <li><a href="lib/staff/teachers.php">Teachers</a></li>
                 </ul>
             </li>
 
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-calendar-check-o" aria-hidden="true"></i>Events<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="../lib/events/dances.php">Dances</a></li>
-                    <li><a href="../lib/events/collegeTrips.php">College Trips</a></li>
-                    <li><a href="../lib/events/bandTrips.php">Band Trips</a></li>
-                    <li><a href="../lib/events/rotcTrips.php">ROTC Trips</a></li>
-                    <li><a href="../lib/events/sportsGames.php">Sports Games</a></li>
-                    <li><a href="../lib/events/gearUp.php">GearUp</a></li>
-                    <li><a href="../lib/events/graduation.php">Graduation</a></li>
+                    <li><a href="lib/events/dances.php">Dances</a></li>
+                    <li><a href="lib/events/collegeTrips.php">College Trips</a></li>
+                    <li><a href="lib/events/bandTrips.php">Band Trips</a></li>
+                    <li><a href="lib/events/rotcTrips.php">ROTC Trips</a></li>
+                    <li><a href="lib/events/sportsGames.php">Sports Games</a></li>
+                    <li><a href="lib/events/gearUp.php">GearUp</a></li>
+                    <li><a href="lib/events/graduation.php">Graduation</a></li>
                 </ul>
             </li>
 
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users" aria-hidden="true"></i>Clubs<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="../lib/clubs/studentCouncil.php">Student Council</a></li>
-                    <li><a href="../lib/clubs/chessClub.php">Chess Club</a></li>
-                    <li><a href="../lib/clubs/ffa.php">FFA</a></li>
-                    <li><a href="../lib/clubs/dineClub.php">Dine Club</a></li>
-                    <li><a href="../lib/clubs/nhs.php">NHS</a></li>
+                    <li><a href="lib/clubs/studentCouncil.php">Student Council</a></li>
+                    <li><a href="lib/clubs/chessClub.php">Chess Club</a></li>
+                    <li><a href="lib/clubs/ffa.php">FFA</a></li>
+                    <li><a href="lib/clubs/dineClub.php">Dine Club</a></li>
+                    <li><a href="lib/clubs/nhs.php">NHS</a></li>
                 </ul>
             </li>
 
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-futbol-o" aria-hidden="true"></i>Sports<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="../lib/sports/baseball.php">Baseball</a></li>
-                    <li><a href="../lib/sports/boysBasketball.php">Boys Basketball</a></li>
-                    <li><a href="../lib/sports/girlsBasketball.php">Girls Basketball</a></li>
-                    <li><a href="sports/cheerleading.php">Cheerleading</a></li>
-                    <li><a href="sports/crossCountry.php">Cross Country</a></li>
-                    <li><a href="sports/football.php">FootBall</a></li>
-                    <li><a href="sports/golf.php">Golf</a></li>
-                    <li><a href="sports/softball.php">SoftBall</a></li>
-                    <li><a href="sports/track.php">Track</a></li>
-                    <li><a href="sports/volleyball.php">VolleyBall</a></li>
-                    <li><a href="sports/wrestling.php">Wrestling</a></li>
+                    <li><a href="lib/sports/baseball.php">Baseball</a></li>
+                    <li><a href="lib/sports/boysBasketball.php">Boys Basketball</a></li>
+                    <li><a href="lib/sports/girlsBasketball.php">Girls Basketball</a></li>
+                    <li><a href="lib/sports/cheerleading.php">Cheerleading</a></li>
+                    <li><a href="lib/sports/crossCountry.php">Cross Country</a></li>
+                    <li><a href="lib/sports/football.php">FootBall</a></li>
+                    <li><a href="lib/sports/golf.php">Golf</a></li>
+                    <li><a href="lib/sports/softball.php">SoftBall</a></li>
+                    <li><a href="lib/sports/track.php">Track</a></li>
+                    <li><a href="lib/sports/volleyball.php">VolleyBall</a></li>
+                    <li><a href="lib/sports/wrestling.php">Wrestling</a></li>
                 </ul>
             </li>
 
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-university" aria-hidden="true"></i>Departments<span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                    <li><a href="departments/administration.php">Administration</a></li>
-                    <li><a href="departments/english.php">English</a></li>
-                    <li><a href="departments/math.php">Mathematics</a></li>
-                    <li><a href="departments/science.php">Science</a></li>
-                    <li><a href="departments/socialStudies.php">Social Studies</a></li>
-                    <li><a href="departments/electives.php">Electives</a></li>
-                    <li><a href="departments/specialEducation.php">Special Education</a></li>
-                    <li><a href="departments/guidance.php">Guidance</a></li>
-                    <li><a href="departments/supprtStaff.php">Support Staff</a></li>
+                    <li><a href="lib/departments/administration.php">Administration</a></li>
+                    <li><a href="lib/departments/english.php">English</a></li>
+                    <li><a href="lib/departments/math.php">Mathematics</a></li>
+                    <li><a href="lib/departments/science.php">Science</a></li>
+                    <li><a href="lib/departments/socialStudies.php">Social Studies</a></li>
+                    <li><a href="lib/departments/electives.php">Electives</a></li>
+                    <li><a href="lib/departments/specialEducation.php">Special Education</a></li>
+                    <li><a href="lib/departments/guidance.php">Guidance</a></li>
+                    <li><a href="lib/departments/supprtStaff.php">Support Staff</a></li>
                 </ul>
             </li>
         </ul>
