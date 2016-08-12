406f9bccd3dbba62c9b1157d83b4b441f1443f34
Charles Sandidge <sandidgec@gmail.com>
2016-08-12 09:15:35 -0600

    added 3 pictures

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             <div class="row">
                 <div class="col-lg-2 col-sm-6">
                     <div class="thumbnail">
-                        <img src="../yearbook/lib/images/chieftains.png">
+                        <img src="http://static.wixstatic.com/media/44891a_f061326c8d0d4d34ad713f98382ec834.jpg_srz_580_430_85_22_0.50_1.20_0.00_jpg_srz">
                     </div>
                 </div>
                 <div class="col-lg-2 col-sm-6">
@@ -37,7 +37,7 @@
                 </div>
                 <div class="col-lg-2 col-sm-6">
                     <div class="thumbnail">
-                        <img src="../yearbook/lib/images/chieftains.png">
+                        <img src="../yearbook/lib/images/logo%20cheifs.jpg">
                     </div>
                 </div>
             </div>
@@ -179,63 +179,31 @@
         <div class="row">
             <div class="col-lg-3 col-sm-6">
                 <div class="thumbnail">
-                    <img src="http://data.whicdn.com/images/233126428/superthumb.jpg">
+                    <img src="http://static.wixstatic.com/media/44891a_f061326c8d0d4d34ad713f98382ec834.jpg_srz_580_430_85_22_0.50_1.20_0.00_jpg_srz">
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6">
                 <div class="thumbnail">
-                    <img src="http://data.whicdn.com/images/233126428/superthumb.jpg">
+                    <img src="../yearbook/lib/images/footballgame.jpg">
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6">
                 <div class="thumbnail">
-                    <img src="http://data.whicdn.com/images/233126428/superthumb.jpg">
+                    <img src="../yearbook/lib/images/guyscreaming.jpg">
                 </div>
             </div>
             <div class="col-lg-3 col-sm-6">
                 <div class="thumbnail">
-                    <img src="http://data.whicdn.com/images/233126428/superthumb.jpg">
-
+                    <img src="../yearbook/lib/images/logochiefs.jpg">
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-3">
-            <div class="media">
-                <div class="media-left media-middle">
-                    <a href="#">
-                        <img class="media-object" src="http://static.wixstatic.com/media/44891a_4b0a37b5491e489889a22510f82a58c6.jpg/v1/fill/w_157,h_259,al_c,q_80,usm_0.66_1.00_0.01/44891a_4b0a37b5491e489889a22510f82a58c6.jpg" alt="...">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <h4 class="page-header">Principal</h4>
-                        <p></p>
-                </div>
-            </div>
-        </div>
 
-        <div class="col-md-3 blue">
-            <div class="media">
-                <div class="media-left media-middle">
-                    <a href="#">
-                        <img class="media-object purple" src="/yearbook/lib/images/chieftains.png" alt="thisIsWhereAnImageGoes">
-                    </a>
-                </div>
-                <div class="media-body purple">
-                    <h3>"Once A Chieftain Always A Chieftain"</h3>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-9">
-            <a href="#" class="thumbnail">
-                <img src="http://static.wixstatic.com/media/44891a_f061326c8d0d4d34ad713f98382ec834.jpg_srz_580_430_85_22_0.50_1.20_0.00_jpg_srz" alt="...">
-            </a>
-        </div>
-    </div>
+
+
+
     <!--<div class="container col-md-2">
         <h2>Information For Yearbooks</h2>
         <p class="two">nsoiv uhns jhv dvndskjv dsvnfkv jv skjvs djkv dsvjsdvkdsn</p>
